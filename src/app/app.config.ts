7bf27b1869b3aceaf6d9824e4d7e41ce4fb2bab4
lucas <lucaston2323@gmail.com>
2026-05-23 22:03:57 -0300

    chore: adicionar dependências como primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+	providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), providePrimeNG({})],
 };
