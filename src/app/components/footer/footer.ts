09430d596b82f1e77415a46c9b8a1a0b247b1f37
lucas <lucaston2323@gmail.com>
2026-06-12 18:40:16 -0300

    feat: add estrutura inicial do footer

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'angular-footer',
+	imports: [],
+	templateUrl: './footer.html',
+})
+export class Footer {}
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+	selector: 'home',
+	imports: [Footer],
+	templateUrl: './home.html',
+})
+export class Home {}
