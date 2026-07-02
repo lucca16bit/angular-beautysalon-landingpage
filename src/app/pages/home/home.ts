0b6cf413a0dc1cafd1b566d3c178efbe08c5b42f
Lucas Gonçalves <103429796+lucca16bit@users.noreply.github.com>
2026-07-02 20:47:22 +0000

    feat: adicionar hero component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../components/footer/footer';
+import { HeroSection } from "../../components/hero-section/hero-section";
 
 @Component({
 	selector: 'home',
-	imports: [Footer],
+	imports: [Footer, HeroSection],
 	templateUrl: './home.html',
 })
 export class Home {}
