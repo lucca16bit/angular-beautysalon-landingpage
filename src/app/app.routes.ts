a8218f1852e92b2ee34caadd1736bb865ed1e36c
Lucas Gonçalves <103429796+lucca16bit@users.noreply.github.com>
2026-07-02 20:46:43 +0000

    feat: adicionar roteamento ao navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		redirectTo: '',
+		pathMatch: 'full',
+	},
+	{
+		path: '',
+		component: Home,
+	},
+	// {
+	// 	path: 'about',
+	// 	component: About,
+	// },
+	// {
+	// 	path: 'contact',
+	// 	component: Contact,
+	// },
+	// {
+	// 	path: 'catalog',
+	// 	component: Catalog,
+	// },
+	// {
+	// 	path: 'schedule',
+	// 	component: Schedule,
+	// },
+	// {
+	// 	path: 'gallery',
+	// 	component: Gallery,
+	// },
+];
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,43 +1,18 @@
 import { Component, HostListener } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'navbar',
-	imports: [],
+	imports: [RouterModule],
 	templateUrl: './navbar.html',
 	styleUrl: './navbar.css',
 })
 export class Navbar {
-	lastScrollPosition: number = 0;
 	rotateChevron: boolean = false;
-	showLangList: boolean = false;
 	menuOpen: boolean = false;
-	isHidden: boolean = false;
 
 	toggleMenu() {
 		this.menuOpen = !this.menuOpen;
 		this.rotateChevron = false;
-		this.showLangList = false;
-	}
-
-	@HostListener('window:scroll', [])
-	handleWindowScroll() {
-		const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-		if (scrollPosition > this.lastScrollPosition) {
-			this.showLangList = false;
-			this.rotateChevron = false;
-			this.isHidden = true;
-		} else {
-			this.isHidden = false;
-		}
-		this.lastScrollPosition = scrollPosition <= 0 ? 0 : scrollPosition;
-	}
-
-	scrollToSection(sectionId: string) {
-		const element = document.getElementById(sectionId);
-		if (element) {
-			element.scrollIntoView({ behavior: 'smooth' });
-		}
-		this.menuOpen = false;
 	}
 }
